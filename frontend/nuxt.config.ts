73a4f7f3c384e696f86410489b920e4915184dfa
nbqduong <nbqduong@gmail.com>
2026-05-15 22:07:23 +0700

    chan ge api

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,9 +1,10 @@
 export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   runtimeConfig: {
     apiInternalBaseUrl: process.env.NUXT_API_INTERNAL_BASE_URL || "http://backend:8080",
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || "http://localhost:8080",
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || "https://NUCLEAR-TEST-API.com",
     },
   },
   app: {
